d8627aa18343f75d66fdcc52a40508e3df7e4f61
Eddy Rivera Echegaray <eddyrivera@Eddys-MacBook-Pro-2.local>
2022-02-19 20:03:10 +0100

    add react router and Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import Header from 'components/Header';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<div>main page</div>} />
+        <Route path="/about" element={<div>Page Description</div>} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
